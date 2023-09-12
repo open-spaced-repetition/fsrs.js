2adc2408f5bc192e9b86359861612b094f8bb72f
oflg <oflg@foxmail.com>
2023-09-12 09:26:21 +0800

    update readme & test

--- README.md
+++ README.md
@@ -13,11 +13,12 @@ npm install fsrs.js
 Create a card and review it at a given time:
 
 ```js
-import * as fsrsjs from 'fsrs.js'
-import { State, Rating } from '../src'
+const fsrsJs = require("fsrs.js")
 
-let fsrs = new fsrsjs.FSRS;
-let card = new fsrsjs.Card;
+let fsrs = new fsrsJs.FSRS;
+let card = new fsrsJs.Card;
+let reating= fsrsJs.Rating;
+let state = fsrsJs.State;
 
 //Set algorithm parameters
 // fsrs.p.w=[0.4, 0.6, 2.4, 5.8, 4.93, 0.94, 0.86, 0.01, 1.49, 0.14, 0.94, 2.18, 0.05, 0.34, 1.26, 0.29, 2.61]
@@ -31,30 +32,30 @@ console.log(scheduling_cards);
 There are four ratings:
 
 ```js
-Rating.Again //incorrect response
-Rating.Hard //recall; correct response recalled with serious difficulty
-Rating.Good //recall; correct response after a hesitation
-Rating.Easy // recall; perfect response
+reating.Again //incorrect response
+reating.Hard //recall; correct response recalled with serious difficulty
+reating.Good //recall; correct response after a hesitation
+reating.Easy // recall; perfect response
 ```
 
 There are four states:
 
 ```js
-State.New //Never been studied
-State.Learning //Been studied for the first time recently
-State.Review //Graduate from learning state
-State.Relearning //Forgotten in review state
+state.New //Never been studied
+state.Learning //Been studied for the first time recently
+state.Review //Graduate from learning state
+state.Relearning //Forgotten in review state
 ```
 
 ```js
 //Get the new state of card for each rating:
-scheduling_cards[Rating.Again].card
-scheduling_cards[Rating.Hard].card
-scheduling_cards[Rating.Good].card
-scheduling_cards[Rating.Easy].card
+scheduling_cards[reating.Again].card
+scheduling_cards[reating.Hard].card
+scheduling_cards[reating.Good].card
+scheduling_cards[reating.Easy].card
 
 //Update the card after rating `Good`:
-card = scheduling_cards[Rating.Good].card
+card = scheduling_cards[reating.Good].card
 
 //Get the due date for card:
 due = card.due
@@ -63,7 +64,7 @@ due = card.due
 state = card.state
 
 //Get the review log after rating `Good`:
-review_log = scheduling_cards[Rating.Good].review_log
+review_log = scheduling_cards[reating.Good].review_log
 ```
 
 ## License
--- test/fsrs.test.ts
+++ test/fsrs.test.ts
@@ -1,131 +1,143 @@
-import * as model from '../src';
-import { Rating, State } from '../src';
-let Card = new model.Card();
-let FSRS = new model.FSRS();
+const fsrsJs = require("fsrs.js")
 
-describe('Card', () => {
-  it('Card.state', () => {
-    expect(Card.state).toEqual(0);
-  });
+let fsrs = new fsrsJs.FSRS;
+let card = new fsrsJs.Card;
+let rating = fsrsJs.Rating;
+let state = fsrsJs.State;
+
+describe('rating', () => {
+    it('rating', () => {
+        expect(rating.Again).toEqual(1);
+    });
 });
 
-describe('FSRS', () => {
-  console.log(FSRS.init_stability(0));
-  it('FSRS.p', () => {
-    expect(FSRS.p.w).toEqual([
-      0.4,
-      0.6,
-      2.4,
-      5.8,
-      4.93,
-      0.94,
-      0.86,
-      0.01,
-      1.49,
-      0.14,
-      0.94,
-      2.18,
-      0.05,
-      0.34,
-      1.26,
-      0.29,
-      2.61,
-    ]);
-  });
+describe('state', () => {
+    it('state', () => {
+        expect(state.New).toEqual(0);
+    });
+});
+
+describe('card', () => {
+    it('card.state', () => {
+        expect(card.state).toEqual(0);
+    });
+});
+
+describe('fsrs', () => {
+    console.log(fsrs.init_stability(0));
+    it('fsrs.p', () => {
+        expect(fsrs.p.w).toEqual([
+            0.4,
+            0.6,
+            2.4,
+            5.8,
+            4.93,
+            0.94,
+            0.86,
+            0.01,
+            1.49,
+            0.14,
+            0.94,
+            2.18,
+            0.05,
+            0.34,
+            1.26,
+            0.29,
+            2.61,
+        ]);
+    });
 });
 
 function test_repeat() {
-  // let rating = new model.Rating();
-  let f = new model.FSRS();
-  f.p.w = [
-    1.14,
-    1.01,
-    5.44,
-    14.67,
-    5.3024,
-    1.5662,
-    1.2503,
-    0.0028,
-    1.5489,
-    0.1763,
-    0.9953,
-    2.7473,
-    0.0179,
-    0.3105,
-    0.3976,
-    0.0,
-    2.0902,
-  ];
-  let card = new model.Card();
-  let revlog: model.ReviewLog;
-  let now = new Date(2022, 10, 29, 12, 30, 0, 0);
-  let scheduling_cards = f.repeat(card, now);
+    let f = new fsrsJs.FSRS;
+    f.p.w = [
+        1.14,
+        1.01,
+        5.44,
+        14.67,
+        5.3024,
+        1.5662,
+        1.2503,
+        0.0028,
+        1.5489,
+        0.1763,
+        0.9953,
+        2.7473,
+        0.0179,
+        0.3105,
+        0.3976,
+        0.0,
+        2.0902,
+    ];
+    let card_test = new fsrsJs.Card();
+    let revlog = new fsrsJs.ReviewLog();
+    let now = new Date(2022, 10, 29, 12, 30, 0, 0);
+    let scheduling_cards = f.repeat(card_test, now);
 
-  console.log(scheduling_cards);
-  let ratings = [
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Again,
-    Rating.Again,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-    Rating.Good,
-  ];
-  let ivl_history = new Array<number>();
-  let state_history = new Array<State>();
-
-  for (let i = 0; i < ratings.length; i++) {
-    card = scheduling_cards[ratings[i]].card;
-    ivl_history.push(card.scheduled_days);
-    revlog = scheduling_cards[ratings[i]].review_log;
-    state_history.push(revlog.state);
-    now = card.due;
-    scheduling_cards = f.repeat(card, now);
     console.log(scheduling_cards);
-  }
+    let ratings = [
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Again,
+        rating.Again,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+        rating.Good,
+    ];
+    let ivl_history = new Array<number>();
+    let state_history = new Array<typeof state>();
 
-  console.log(ivl_history);
-  console.log(state_history);
+    for (let i = 0; i < ratings.length; i++) {
+        card_test = scheduling_cards[ratings[i]].card;
+        ivl_history.push(card_test.scheduled_days);
+        revlog = scheduling_cards[ratings[i]].review_log;
+        state_history.push(revlog.state);
+        now = card_test.due;
+        scheduling_cards = f.repeat(card_test, now);
+        console.log(scheduling_cards);
+    }
 
-  expect(ivl_history).toEqual([
-    0,
-    5,
-    16,
-    43,
-    106,
-    236,
-    0,
-    0,
-    12,
-    25,
-    47,
-    85,
-    147,
-  ]);
+    console.log(ivl_history);
+    console.log(state_history);
+    console.log(card_test);
 
-  expect(state_history).toEqual([
-    State.New,
-    State.Learning,
-    State.Review,
-    State.Review,
-    State.Review,
-    State.Review,
-    State.Review,
-    State.Relearning,
-    State.Relearning,
-    State.Review,
-    State.Review,
-    State.Review,
-    State.Review,
-  ]);
+    expect(ivl_history).toEqual([
+        0,
+        5,
+        16,
+        43,
+        106,
+        236,
+        0,
+        0,
+        12,
+        25,
+        47,
+        85,
+        147,
+    ]);
+
+    expect(state_history).toEqual([
+        state.New,
+        state.Learning,
+        state.Review,
+        state.Review,
+        state.Review,
+        state.Review,
+        state.Review,
+        state.Relearning,
+        state.Relearning,
+        state.Review,
+        state.Review,
+        state.Review,
+        state.Review,
+    ]);
 }
 
 test_repeat();
-
-console.log(Card);
